166f73fec5a0a3db924b5b44550b521fa2d389a9
Leonardo Uriza <ur.leo16@gmail.com>
2024-02-28 21:37:16 -0500

    Se crean los componentes footer y portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent]
 })
 export class AppComponent {
   title = 'portafolio';
