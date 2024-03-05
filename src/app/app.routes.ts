b6c85843c1dce2eadb4c6c868fbf25cec7e0f739
Leonardo Uriza <ur.leo16@gmail.com>
2024-03-04 21:14:27 -0500

    se configuran los routerLink y el hash location

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 export const routes: Routes = [
-    {path: '', component: PortafolioComponent},
+    {path: 'home', component: PortafolioComponent},
     {path: 'about', component: AboutComponent},
     {path: 'item', component: ItemComponent},
-    {path: '**', pathMatch: 'full', redirectTo: ''},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'},
 ];
 
 
